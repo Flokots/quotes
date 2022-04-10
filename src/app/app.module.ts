68d7c70b0b0e6faa95c4f954d8f79666aeee5253
FloKots <florencekotohoyoh@gmail.com>
2022-04-10 12:38:35 +0300

    add quote class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesDisplayComponent } from './components/quotes-display/quotes-display.component';
+import { QuotesDetailsComponent } from './components/quotes-details/quotes-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesDisplayComponent
+    QuotesDisplayComponent,
+    QuotesDetailsComponent
   ],
   imports: [
     BrowserModule,
