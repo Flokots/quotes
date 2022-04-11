0ffd85053b17722d9e86ec7603c2f55cdf46ee53
FloKots <florencekotohoyoh@gmail.com>
2022-04-11 07:07:05 +0300

    add showDetails property to quote blueprint class

--- src/app/model/quote.ts
+++ src/app/model/quote.ts
@@ -1,8 +1,11 @@
 export class Quote {
+  showDetails: boolean;
   constructor(
     public id: number,
     public description: string,
     public author: string,
     public submitter: string
-  ){}
+  ){
+    this.showDetails=false;
+  }
 }
